49e7d11038bd1d7e8ed684e47d537d23dc8f89f3
Premdeep Gill <premdeepgill@users.noreply.github.com>
2017-06-12 12:02:00 +0530

    Chapter 2 code examples

---
+++ ch02/comments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.5
+# This is a comment.
+# This is also a comment.
+# This is yet another comment.
+
+"""
+This is a comment.
+This is also a comment.
+This is yet another comment.
+"""
---
+++ ch02/helloworld.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.5
+# Prints the words "Hello World"
+
+print("Hello World")
