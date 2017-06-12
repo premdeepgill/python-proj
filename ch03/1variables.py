705d52354cf4ee7439af2c8f09d32368c401266c
Premdeep Gill <premdeepgill@users.noreply.github.com>
2017-06-12 13:01:26 +0530

    1variables.py and 3assignOperator.py committed

---
+++ ch03/1variables.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.5
+
+userAge, userName = 26, "Premdeep Gill"
+
+print(userAge)
+print(userName)
---
+++ ch03/3assignOperator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.5
+
+x = 5
+y = 10
+x = y
+
+print("x = ", x)
+print("y = ", y)
